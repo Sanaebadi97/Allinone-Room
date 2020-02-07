cd2defe04a30b351c1c45c15b245b72a3778e98e
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-07 17:51:42 +0330

    init commit

---
+++ app/src/main/java/com/pixelnx/sam/allinone_room/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pixelnx.sam.allinone_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
