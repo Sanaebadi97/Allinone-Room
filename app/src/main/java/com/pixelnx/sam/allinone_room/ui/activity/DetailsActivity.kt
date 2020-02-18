e73aa9abc2b6f87fb138d3c9bd998890ef687c8e
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-18 14:37:37 +0330

    send bundle to next activity with log

--- app/src/main/java/com/pixelnx/sam/allinone_room/ui/activity/DetailsActivity.kt
+++ app/src/main/java/com/pixelnx/sam/allinone_room/ui/activity/DetailsActivity.kt
@@ -1,13 +1,26 @@
 package com.pixelnx.sam.allinone_room.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.pixelnx.sam.allinone_room.R
+import com.pixelnx.sam.allinone_room.model.Note
+import com.pixelnx.sam.allinone_room.util.Const
 
 class DetailsActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "DetailsActivity"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        if (intent.hasExtra(Const.SELECTED_NOTE)) {
+            val note = intent.getParcelableExtra<Note>(Const.SELECTED_NOTE)
+            Log.i(TAG, "TITLE IS ${note.title} Date IS ${note.timeStamp}")
+        }
     }
 }
