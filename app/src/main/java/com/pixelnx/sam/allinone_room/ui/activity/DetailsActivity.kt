b8c214bbc773e6b23d134241ed4769a2f20d6dc9
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-18 13:41:18 +0330

    added Details Activity

---
+++ app/src/main/java/com/pixelnx/sam/allinone_room/ui/activity/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.pixelnx.sam.allinone_room.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pixelnx.sam.allinone_room.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
