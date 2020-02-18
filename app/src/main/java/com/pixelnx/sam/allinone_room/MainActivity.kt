dd61c13fca51629eeace52a26fdef49f0cc115df
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-18 11:14:01 +0330

    added recycler view with fake data and shown the list

--- app/src/main/java/com/pixelnx/sam/allinone_room/MainActivity.kt
+++ app/src/main/java/com/pixelnx/sam/allinone_room/MainActivity.kt
@@ -2,7 +2,11 @@ package com.pixelnx.sam.allinone_room
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pixelnx.sam.allinone_room.adapter.NoteAdapter
 import com.pixelnx.sam.allinone_room.model.Note
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +14,35 @@ class MainActivity : AppCompatActivity() {
         const val TAG: String = "MainActivity"
     }
 
+    // lateinit var recyclerView: RecyclerView
+    private val noteList: MutableList<Note> = ArrayList<Note>()
+    lateinit var noteAdapter: NoteAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val note = Note("jhujdv", "bvghd", "kjhujd")
-        note.title=""
+        // recyclerView = findViewById(R.id.rv_note)
 
+        rvConfig()
+        insertFakeData()
+
+
+    }
+
+    private fun rvConfig() {
+        rv_note.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rv_note.setHasFixedSize(true)
+        noteAdapter = NoteAdapter(noteList)
+        rv_note.adapter = noteAdapter
+    }
+
+    private fun insertFakeData() {
+        for (i in 0..100) {
+            val note = Note("Title $i", "Content $i", "Date $i")
+            noteList.add(note)
+        }
+        noteAdapter.notifyDataSetChanged()
     }
 }
