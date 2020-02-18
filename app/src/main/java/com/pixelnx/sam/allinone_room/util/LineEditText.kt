f26d37318bc29f60ae35d6bd838a06fd6f312683
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-18 17:30:51 +0330

    added onDraw and canvas

--- app/src/main/java/com/pixelnx/sam/allinone_room/util/LineEditText.kt
+++ app/src/main/java/com/pixelnx/sam/allinone_room/util/LineEditText.kt
@@ -10,8 +10,9 @@ import androidx.appcompat.widget.AppCompatEditText
 
 class LineEditText(context: Context?) : AppCompatEditText(context) {
 
-    lateinit var mRect: Rect
-    lateinit var mPaint: Paint
+    private var mRect: Rect = Rect()
+    private var mPaint: Paint = Paint()
+
 
     override fun onDraw(canvas: Canvas?) {
         val height = (parent as View).height
@@ -21,13 +22,16 @@ class LineEditText(context: Context?) : AppCompatEditText(context) {
         val rect: Rect = mRect
         val paint: Paint = mPaint
 
-        val baseLine = getLineBounds(0, rect)
+        mPaint.style = Paint.Style.STROKE
+
+        var baseLine = getLineBounds(0, rect)
         for (i in 0..numberOfLines) {
 
             canvas!!.drawLine(
                 rect.left.toFloat(),
                 (baseLine + 1).toFloat(), rect.right.toFloat(), (baseLine + 1).toFloat(), paint
             )
+            baseLine += lineHeight
         }
         super.onDraw(canvas)
     }
