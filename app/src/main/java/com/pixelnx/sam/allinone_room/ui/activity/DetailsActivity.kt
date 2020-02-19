267cf3e9e009c84820645030d818206c0a2948df
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-19 17:29:27 +0330

    added OnTouchListener

--- app/src/main/java/com/pixelnx/sam/allinone_room/ui/activity/DetailsActivity.kt
+++ app/src/main/java/com/pixelnx/sam/allinone_room/ui/activity/DetailsActivity.kt
@@ -3,6 +3,9 @@ package com.pixelnx.sam.allinone_room.ui.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pixelnx.sam.allinone_room.R
 import com.pixelnx.sam.allinone_room.model.Note
@@ -10,7 +13,7 @@ import com.pixelnx.sam.allinone_room.util.Const
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.layout_note_toolbar.*
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity(), View.OnTouchListener ,GestureDetector.OnGestureListener{
 
     companion object {
         const val TAG: String = "DetailsActivity"
@@ -18,6 +21,7 @@ class DetailsActivity : AppCompatActivity() {
 
     private var isNewNotes: Boolean? = false
     private var note: Note? = null
+    private var mGestureDetector: GestureDetector? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,11 @@ class DetailsActivity : AppCompatActivity() {
             setNoteProperties()
         }
 
+    }
+
+    private fun setListener() {
+        note_text.setOnTouchListener(this)
+        mGestureDetector= GestureDetector(this,this)
     }
 
     private fun getIncomingIntent(): Boolean {
@@ -60,5 +69,43 @@ class DetailsActivity : AppCompatActivity() {
     private fun setNewNoteProperties() {
         note_text_title.text = getString(R.string.note_title)
         note_edit_title.setText(getString(R.string.note_title))
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
